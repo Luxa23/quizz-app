3ea085d74300f204bcad163a96911b7d7429291c
Anna Rolfes <anna-rolfes@gmx.de>
2022-04-14 17:03:55 +0200

    Add husky, stylelint, eslint, lintstaged

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const bookmark = document.querySelector("[data-js = 'svg--bookmark']");
 
-bookmark.addEventListener("click", () => {
-  bookmark.classList.toggle("bookmark--active");
+bookmark.addEventListener('click', () => {
+  bookmark.classList.toggle('bookmark--active');
   console.log(bookmark);
 });
 
 const question = document.querySelector("[data-js = 'question']");
 const buttonShowAnswer = document.querySelector("[data-js = 'showAnswer']");
 
-buttonShowAnswer.addEventListener("click", () => {
-  question.classList.toggle("card__answer--show");
+buttonShowAnswer.addEventListener('click', () => {
+  question.classList.toggle('card__answer--show');
   console.log(question);
 });
